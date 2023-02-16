5a10b509a1888f61dcc0ecbad402cf6b72f8f327
notarious1~git config --global user.email notarious2@gmail.comgit config --global user.name notarious <notarious2@gmail.com>
2023-02-16 17:10:41 +0500

    started adding test, need to add github actions

---
+++ backend/task/tests/test_models.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from task.models import Task
+from user.models import User
+
+
+class TestTaskModel(TestCase):
+    """Test task model"""
+
+    def test_create_task_user(self):
+        """Test task creation by registered user"""
+        user = User.objects.create_user(
+            email="test@example.com", username="testuser")
+        task = Task.objects.create(priority=1, text="test task", user=user)
+        self.assertEqual(Task.objects.count(), 1)
+        self.assertEqual(task.user, user)
+        self.assertEqual(str(task), "test task by testuser")
